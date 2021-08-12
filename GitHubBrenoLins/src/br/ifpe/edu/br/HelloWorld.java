bc1dfb882fed29da252e1d4b8de1e1095ecf0a56
Breno Lins <brenoh21@gmail.com>
2021-08-12 20:05:15 -0300

    Teste

--- GitHubBrenoLins/src/br/ifpe/edu/br/HelloWorld.java
+++ GitHubBrenoLins/src/br/ifpe/edu/br/HelloWorld.java
@@ -4,10 +4,10 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 
-
+		String nome2 ="Breno H";
 		String nome = "Breno Lins";
 		System.out.println(nome);
-
+		System.out.println(nome2);
 				
 	}
 
