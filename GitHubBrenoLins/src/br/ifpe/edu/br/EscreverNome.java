1235537d43352d8a91e6f7d865329aa77e286228
Breno Lins <brenoh21@gmail.com>
2021-09-22 20:14:09 -0300

    Terceiro commit

--- GitHubBrenoLins/src/br/ifpe/edu/br/EscreverNome.java
+++ GitHubBrenoLins/src/br/ifpe/edu/br/EscreverNome.java
@@ -6,11 +6,11 @@ public class EscreverNome {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		System.out.println("Qual seu nome?");
-		String nome = "Bruno";
+		String nome3 = "Bruno";
 		String nome2;
 		nome2 = ler.next();
 		System.out.println("Seu nome é: "+nome2);
-		System.out.println("Seu nome é: "+nome);
+		System.out.println("Seu nome é: "+nome3);
 		
 	}
 
